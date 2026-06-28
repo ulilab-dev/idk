a8a85619d7f59e38177c27d450e87c9b2f1e230b
ulil <ullia5733@gmail.com>
2026-06-29 02:06:26 +0700

    arch-btw

---
+++ script/aurpkgs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+sudo pacman -Sy networkmanager \
+  git \
+  base-devel \
+  fastfetch \
+  htop \
+  btop \
+  fish \
+  power-profiles-daemon \
+  dolphin \
+  ark \
+  konsole \
+  kate \
+  gwenview \
+  haruna \
+  ttf-jetbrains-mono-nerd \
+  opencl-nvidia \
+  lib32-opencl-nvidia \
+  lib32-nvidia-utils \
+  nvidia-settings \
+  cmus \
+  micro
