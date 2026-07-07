dc394a8d05258a94bea381a3e4ad229398617b02
ulil <ullia5733@gmail.com>
2026-07-07 23:41:42 +0700

    update

---
+++ void/script/alluneeddwl.sh
@@ -0,0 +1,43 @@
+#!/bin/sh
+
+sudo xbps-install -S wlroots \
+                     wlr-randr \
+                     waybar \
+                     rofi \
+                     rofi-emoji \
+                     flatpak \
+                     dunst \
+                     vim \
+                     micro \
+                     qt5-wayland \
+                     qt6-wayland \
+                     qt6ct \
+                     nwg-look \
+                     cliphist \
+                     wl-clipboard \
+                     wl-clip-persist \
+                     htop \
+                     btop \
+                     baobab \
+                     dolphin \
+                     kate \
+                     haruna \
+                     gwenview \
+                     kde-cli-tools \
+                     polkit-gnome \
+                     power-profiles-daemon \
+                     networkmanager \
+                     networkmanager-dmenu \
+                     grim \
+                     slurp \
+                     swaylock \
+                     awww \
+                     python3-pipx \
+                     git \
+                     base-devel \
+                     fish-shell \
+                     eza \
+                     starship \
+                     
+                     
+                     
