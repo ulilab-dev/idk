e320e7e97e4bed9ea577ee1f2b60152565d3618f
ulil <ullia5733@gmail.com>
2026-07-08 00:14:07 +0700

    add nvidia

---
+++ void/script/install_nvidia.sh
@@ -0,0 +1,12 @@
+#!/bin/sh 
+
+sudo xbps-install -Syu nvidia-firmware \ 
+                       nvidia \
+                       nvidia-libs \
+                       nvidia-opencl \
+                       nvidia-gtklibs \
+                       nvidia-libs-32bit \
+                       nvidia-opencl-32bit \
+                       nvidia-gtklibs-32bit \
+ 
+                       
