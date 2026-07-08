ff3d9a409b25605799e45e29d9eab736d2356f9e
ulil <ullia5733@gmail.com>
2026-07-08 16:43:10 +0700

    add hyprland

---
+++ home_pc/arch/script/alluneedhyprland.sh
@@ -0,0 +1,48 @@
+#!/bin/sh
+
+sudo pacman -Sy hyprland \
+                hyprland-qt-support \
+                xdg-desktop-portal-hyprland \
+                git \
+                base-devel \
+                flatpak \
+                fish \
+                eza \
+                starship \
+                ttf-jetbrains-mono-nerd \
+                kitty \
+                vim \
+                micro \
+                rofi \
+                rofi-emoji \
+                waybar \
+                hyprshot \
+                hyprlock \
+        		dunst \
+	        	qt5-wayland \
+	         	qt6-wayland \
+                qt6ct \
+                nwg-look \
+                adw-gtk-theme \
+                awww \
+                networkmanager-dmenu \
+                networkmanager \
+                cliphist \
+                wl-clipboard \
+                wl-clip-persist \
+                ffmpegthumbs \
+                pavucontrol \
+                fastfetch \
+                htop \
+                btop \
+                baobab \
+                dolphin \
+                ark \
+                kate \
+                haruna \
+                gwenview \
+                archlinux-xdg-menu \
+                kde-cli-tools \
+                power-profiles-daemon \
+                polkit-gnome 
+                
