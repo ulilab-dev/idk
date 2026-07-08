9e8ad1b53971bb8d1044419c3cbe39448e826d24
ulil <ullia5733@gmail.com>
2026-07-08 16:31:28 +0700

    remove

--- home_pc/arch/script/install-nvidia.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-sudo pacman -Sy nvidia-open-lts \
-                nvidia-utils \
-                lib32-nvidia-utils \
-                lib32-opencl-nvidia \
-                opencl-nvidia \
-                nvidia-settings 
-                
